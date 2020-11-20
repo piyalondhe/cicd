e6e96a7927ff281ed258601f6f97d4c79df1aa6a
piyalondhe <61679440+piyalondhe@users.noreply.github.com>
2020-11-20 12:57:52 +0530

    update1

--- EvenOdd.java
+++ EvenOdd.java
@@ -4,10 +4,11 @@ public class EvenOdd {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+       // Scanner reader = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int num = reader.nextInt();
+       // System.out.print("Enter a number: ");
+       // int num = reader.nextInt();
+        int num=10;
 
         if(num % 2 == 0)
             System.out.println(num + " is even");
